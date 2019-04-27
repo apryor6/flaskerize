5c2a51438617e11b0ef7a39313946c43e846bd08
Alan Pryor <APryor@ATD-US.com>
2019-04-27 07:05:28 -0400

    Add test for dry run

--- flaskerize/parser_test.py
+++ flaskerize/parser_test.py
@@ -1,4 +1,11 @@
 def test_generate():
     import os
-    status = os.system('flaskerize --dry-run -g hw test')
+    status = os.system('flaskerize --dry-run -g hw should_not_create.py')
     assert status == 0
+
+
+def test_dry_run():
+    import os
+    status = os.system('flaskerize --dry-run -g hw should_not_create.py')
+    assert status == 0
+    assert not os.path.isfile('should_not_create.py')
