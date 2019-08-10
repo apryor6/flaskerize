a895652347435063d8d8de4d490c9a56d4927594
AJ Pryor <apryor6@gmail.com>
2019-08-10 11:26:14 -0400

    Refactor FzArgumentParser to start with a global schema

--- flaskerize/parser.py
+++ flaskerize/parser.py
@@ -1,12 +1,9 @@
 import os
 import argparse
 import sys
-from typing import List, Optional
+from typing import Dict, List, Optional
 
-from .exceptions import InvalidSchema
 
-# dirname = os.path.dirname(__file__)
-# global_schema = os.path.join(dirname, 'global/schema.json')
 # global_arg_parser = from_schema(global_schema)
 # schematic_arg_parser = from_schema("global/schema.json")
 # arg_parser = global_arg_parser
@@ -34,57 +31,82 @@ from .exceptions import InvalidSchema
 # )
 
 
-def from_schema(filename: str) -> argparse.ArgumentParser:
-    """Create an argument parser from a schema JSON file"""
+def _convert_types(cfg: Dict) -> Dict:
+    for option in cfg["options"]:
+        if "type" in option:
+            option["type"] = _translate_type(option["type"])
+    return cfg
 
+
+def _translate_type(key: str) -> type:
+    """Convert type name from JSON schema to corresponding Python type"""
+
+    type_map: Dict[str, type] = {"str": str}
+    return type_map[key]
+
+
+def _load_schema(filename: str) -> Dict:
     import json
+
+    from .exceptions import InvalidSchema
 
     with open(filename, "r") as fid:
         cfg = json.load(fid)
     if "options" not in cfg:
         raise InvalidSchema(f"Required key 'options' not found in '{filename}'")
-    arg_parser = argparse.ArgumentParser(description="Flaskerize")
-    for option in cfg["options"]:
-        arg_parser.add_argument(
-            "--dry-run",
-            action="store_true",
-            help="Dry run -- don't actually create any files.",
-        )
-    return arg_parser
+
+    cfg = _convert_types(cfg)
+    return cfg
+
+
+# def _from_schema(filename: str) -> argparse.ArgumentParser:
+#     """Create an argument parser from a schema JSON file"""
+
+#     cfg = _load_schema(filename)
+#     arg_parser = argparse.ArgumentParser(description="Flaskerize")
+#     for option in cfg["options"]:
+#         arg_parser.add_argument(
+#             "--dry-run",
+#             action="store_true",
+#             help="Dry run -- don't actually create any files.",
+#         )
+#     return arg_parser
 
 
 def parse(args):
     return arg_parser.parse_args(args)
 
 
+dirname = os.path.dirname(__file__)
+
+
 class FzArgumentParser(argparse.ArgumentParser):
     """Flaskerize argument parser with default common options"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.add_argument(
-            "--force",
-            "-f",
-            action="store_true",
-            help="Ignore safety checks, such as checking that "
-            "target Flask app is a *.py",
-        )
-        self.add_argument(
-            "--dry-run",
-            action="store_true",
-            help="Dry run -- don't actually create any files.",
-        )
+    _GLOBAL_SCHEMA = os.path.join(dirname, "global/schema.json")
+
+    def __init__(self, schema_files: Optional[List[str]] = None):
+        import json
+
+        super().__init__()
+        cfgs: List[Dict] = [_load_schema(self._GLOBAL_SCHEMA)]
+        if schema_files:
+            cfgs.extend([_load_schema(file) for file in schema_files])
+
+        for cfg in cfgs:
+            for option in cfg["options"]:
+                switches = [option.pop("arg")] + option.pop("aliases", [])
+                self.add_argument(*switches, **option)
 
 
 class Flaskerize(object):
     COMMANDS: List[str] = ["attach", "bundle", "generate"]
 
     def __init__(self, args):
-        parser = FzArgumentParser(
-            description="Build tool Command line interface (CLI) for Flask",
-            usage="""flaskerize <command> [<args>]
-            """,
-        )
+        import os
+
+        dirname = os.path.dirname(__file__)
+        parser = FzArgumentParser()
         parser.add_argument("command", help="Subcommand to run")
         parsed = parser.parse_args(args[1:2])
         if not hasattr(self, parsed.command):
@@ -100,84 +122,84 @@ class Flaskerize(object):
         parser.print_help()
         exit(1)
 
-    def attach(self, args):
-        from flaskerize.attach import attach
+    # def attach(self, args):
+    #     from flaskerize.attach import attach
 
-        arg_parser = FzArgumentParser(description="attach [a]")
-        arg_parser.add_argument(
-            "-to",
-            type=str,
-            required=True,
-            help="Flask app factory function to attach blueprint",
-        )
-        arg_parser.add_argument("bp", type=str, help="Blueprint to attach")
-        parse = arg_parser.parse_args(args)
-        attach(parse)
+    #     arg_parser = FzArgumentParser(description="attach [a]")
+    #     arg_parser.add_argument(
+    #         "-to",
+    #         type=str,
+    #         required=True,
+    #         help="Flask app factory function to attach blueprint",
+    #     )
+    #     arg_parser.add_argument("bp", type=str, help="Blueprint to attach")
+    #     parse = arg_parser.parse_args(args)
+    #     attach(parse)
 
-    def bundle(self, args):
-        """
-        Generate a new Blueprint from a source static site and attach it
-        to an existing Flask application
-        """
-        import os
+    # def bundle(self, args):
+    #     """
+    #     Generate a new Blueprint from a source static site and attach it
+    #     to an existing Flask application
+    #     """
+    #     import os
 
-        DEFAULT_BP_NAME = "_fz_bp.py"
-        DEFAULT_WSGI_NAME = "wsgi.py"
-        DEFAULT_GUNICORN_ENTRY = f"{DEFAULT_WSGI_NAME.replace('.py', '')}:app"
+    #     DEFAULT_BP_NAME = "_fz_bp.py"
+    #     DEFAULT_WSGI_NAME = "wsgi.py"
+    #     DEFAULT_GUNICORN_ENTRY = f"{DEFAULT_WSGI_NAME.replace('.py', '')}:app"
 
-        arg_parser = FzArgumentParser(description="bundle [b]")
-        arg_parser.add_argument(
-            "-from", "--source", type=str, help="Path of input static site to bundle"
-        )
-        arg_parser.add_argument(
-            "-to",
-            type=str,
-            required=True,
-            help="Flask app factory function to attach blueprint",
-        )
-        arg_parser.add_argument(
-            "--with-wsgi",
-            action="store_true",
-            help="Also generate a wsgi.py for gunicorn",
-        )
-        arg_parser.add_argument(
-            "--with-dockerfile", action="store_true", help="Also generate a Dockerfile"
-        )
-        parsed = arg_parser.parse_args(args)
-        if parsed.force:
-            force = "--force"
-        else:
-            force = ""
+    #     arg_parser = FzArgumentParser(description="bundle [b]")
+    #     arg_parser.add_argument(
+    #         "-from", "--source", type=str, help="Path of input static site to bundle"
+    #     )
+    #     arg_parser.add_argument(
+    #         "-to",
+    #         type=str,
+    #         required=True,
+    #         help="Flask app factory function to attach blueprint",
+    #     )
+    #     arg_parser.add_argument(
+    #         "--with-wsgi",
+    #         action="store_true",
+    #         help="Also generate a wsgi.py for gunicorn",
+    #     )
+    #     arg_parser.add_argument(
+    #         "--with-dockerfile", action="store_true", help="Also generate a Dockerfile"
+    #     )
+    #     parsed = arg_parser.parse_args(args)
+    #     if parsed.force:
+    #         force = "--force"
+    #     else:
+    #         force = ""
 
-        if parsed.source and not parsed.source.endswith("/"):
-            print(
-                f"Input source {parsed.source} does not end with trailing /, adding "
-                "for you"
-            )
-            parsed.source += "/"
-        self.generate(
-            f"blueprint -from {parsed.source} {DEFAULT_BP_NAME} {force}".split()
-        )
-        self.attach(f"-to {parsed.to} {DEFAULT_BP_NAME} {force}".split())
+    #     if parsed.source and not parsed.source.endswith("/"):
+    #         print(
+    #             f"Input source {parsed.source} does not end with trailing /, adding "
+    #             "for you"
+    #         )
+    #         parsed.source += "/"
+    #     self.generate(
+    #         f"blueprint -from {parsed.source} {DEFAULT_BP_NAME} {force}".split()
+    #     )
+    #     self.attach(f"-to {parsed.to} {DEFAULT_BP_NAME} {force}".split())
 
-        # Build a WSGI file if requested or needed. If user has requested a Dockerfile
-        # without adding --with-wsgi flaskerize will add one unless a wsgi.py exists
-        # already
-        if parsed.with_wsgi or (
-            parsed.with_dockerfile and not os.path.isfile(f"{DEFAULT_WSGI_NAME}")
-        ):
-            self.generate(f"wsgi -from {parsed.to} {DEFAULT_WSGI_NAME} {force}".split())
+    #     # Build a WSGI file if requested or needed. If user has requested a Dockerfile
+    #     # without adding --with-wsgi flaskerize will add one unless a wsgi.py exists
+    #     # already
+    #     if parsed.with_wsgi or (
+    #         parsed.with_dockerfile and not os.path.isfile(f"{DEFAULT_WSGI_NAME}")
+    #     ):
+    #         self.generate(f"wsgi -from {parsed.to} {DEFAULT_WSGI_NAME} {force}".split())
 
-        if parsed.with_dockerfile:
-            self.generate(
-                f"dockerfile Dockerfile {force} -from {DEFAULT_GUNICORN_ENTRY}".split()
-            )
+    #     if parsed.with_dockerfile:
+    #         self.generate(
+    #             f"dockerfile Dockerfile {force} -from {DEFAULT_GUNICORN_ENTRY}".split()
+    #         )
 
     def generate(self, args):
         from flaskerize import generate
         import os
 
-        arg_parser = FzArgumentParser(description="generate [g]")
+        arg_parser = FzArgumentParser()
         arg_parser.add_argument("what", type=str, help="What to generate")
         arg_parser.add_argument(
             "output_name",
