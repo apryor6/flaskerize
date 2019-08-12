9c31159dd0ce7b93b4fd4ba4de6756b6692f27be
AJ Pryor <apryor6@gmail.com>
2019-08-12 09:17:46 -0400

    Increase to 100% test coverage on generate.py

--- flaskerize/generate.py
+++ flaskerize/generate.py
@@ -262,17 +262,11 @@ def dockerfile(args):
     print("args = ", args)
     import os
 
-    if os.path.isfile("requirements.txt"):
-        req_txt = """COPY requirements.txt /requirements.txt
-RUN pip install --install-option="--prefix=/install" -r /requirements.txt"""
-    else:
-        req_txt = ""
     CONTENTS = f"""FROM python:3.7 as base
 
 FROM base as builder
 RUN mkdir /install
 WORKDIR /install
-{req_txt}
 RUN pip install --install-option="--prefix=/install" gunicorn
 RUN pip install --install-option="--prefix=/install" flask
 
--- flaskerize/generate_test.py
+++ flaskerize/generate_test.py
@@ -35,10 +35,108 @@ def test_app_from_dir(_generate: MagicMock):
     _generate.assert_called_once()
 
 
-# @patch("flaskerize.generate._generate")
-# def test_app(_generate: MagicMock):
-#     from flaskerize.generate import app
+@patch("flaskerize.generate._generate")
+def test_wsgi(_generate: MagicMock):
+    from flaskerize.generate import wsgi
 
-#     app([])
+    @dataclass
+    class Args:
+        dry_run: bool = True
+        output_name: str = "output_name"
+        output_file: str = "output_file"
+        filename: str = "filename"
+        source: str = "/path/to/source"
 
-#     _generate.assert_called_once()
+    wsgi(Args())
+
+    _generate.assert_called_once()
+
+
+@patch("flaskerize.generate._generate")
+def test_namespace(_generate: MagicMock):
+    from flaskerize.generate import namespace
+
+    @dataclass
+    class Args:
+        dry_run: bool = True
+        output_name: str = "output_name"
+        output_file: str = "output_file"
+        filename: str = "filename"
+        source: str = "/path/to/source"
+        without_test: bool = False
+
+    namespace(Args())
+
+    _generate.assert_called()
+
+
+@patch("flaskerize.generate._generate")
+def test_namespace_test(_generate: MagicMock):
+    from flaskerize.generate import namespace_test
+
+    @dataclass
+    class Args:
+        dry_run: bool = True
+        output_name: str = "output_name"
+        output_file: str = "output_file"
+        filename: str = "filename"
+        source: str = "/path/to/source"
+        without_test: bool = False
+
+    namespace_test(Args())
+
+    _generate.assert_called()
+
+
+@patch("flaskerize.generate._generate")
+def test_dockerfile(_generate: MagicMock):
+    from flaskerize.generate import dockerfile
+
+    @dataclass
+    class Args:
+        dry_run: bool = True
+        output_name: str = "output_name"
+        output_file: str = "output_file"
+        filename: str = "filename"
+        source: str = "/path/to/source"
+        without_test: bool = False
+
+    dockerfile(Args())
+
+    _generate.assert_called()
+
+
+def test__generate_with_dry_run(tmp_path):
+    from os import path
+
+    from flaskerize.generate import _generate
+
+    CONTENTS = "asdf"
+    output_name = f"{tmp_path}/some/file"
+    _generate(contents=CONTENTS, output_name=output_name, dry_run=True)
+
+    assert not path.isfile(output_name)
+
+
+def test__generate_with_file(tmp_path):
+    from os import path
+
+    from flaskerize.generate import _generate
+
+    CONTENTS = "asdf"
+    output_name = f"{tmp_path}/file.py"
+    _generate(contents=CONTENTS, output_name=output_name, dry_run=False)
+
+    assert path.isfile(output_name)
+
+
+def test__generate_with_adds_extension(tmp_path):
+    from os import path
+
+    from flaskerize.generate import _generate
+
+    CONTENTS = "asdf"
+    output_name = f"{tmp_path}/file"
+    _generate(contents=CONTENTS, output_name=output_name, dry_run=False)
+
+    assert path.isfile(output_name + ".py")
