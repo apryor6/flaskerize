d71eb24e6414bd940674ea5b0c470434360fa88d
AJ Pryor <apryor6@gmail.com>
2019-08-15 14:33:16 -0400

    Update MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flaskerize",
-    version="0.3.2",
+    version="0.3.3",
     description="Flask CLI build/dev tool for bundling static sites into Flask apps and templated code generation",
     author="AJ Pryor",
     author_email="apryor6@gmail.com",
