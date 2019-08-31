b695689b5465775cc7b235396d6b172e6177a4b6
AJ Pryor <apryor6@gmail.com>
2019-08-31 16:27:45 -0400

    Fix a test that was rendering a local directory

--- flaskerize/schematics/schematic/schematic_test.py
+++ flaskerize/schematics/schematic/schematic_test.py
@@ -4,4 +4,6 @@ import os
 def test_schematic_from_Flaskerize(tmp_path):
     from flaskerize.parser import Flaskerize
 
-    assert Flaskerize(f"fz generate schematic {tmp_path}".split())
+    assert Flaskerize(
+        f"fz generate schematic --from-dir {tmp_path} test_schematic".split()
+    )
